11d3dad2944d570e30da83bc4ef41fee83826a48
nstudebaker@watchpointdata.com <nstudebaker@watchpointdata.com>
2017-01-03 10:23:02 -0800

    Bulk create users in active directory
    Create active directory user objects from a pre-filled csv.

---
+++ active-directory-bulk-create-users.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.Title
+Bulk create active directory users
+.Description
+Bulk create active directory users based on the values of a csv file.
+.How To Use
+The csv should contain the following headers
+Name,GivenName,Surname,SamAccountName,UserPrincipalName
+.Created By
+Nathan Studebaker
+#>
+
+#Import the csv and create the users
+Import-Csv C:\support\user-list.csv | New-ADUser -PassThru | `
+Set-ADAccountPassword -Reset ` -NewPassword (ConvertTo-SecureString -AsPlainText 'Pa$$w0rd01' -Force) `
+-PassThru | Enable-ADAccount #Be mindful off the ' in the two lines above
+
